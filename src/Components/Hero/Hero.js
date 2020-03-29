c9f07ab24504bb86be0bb7e53d629e556d1e922c
Christian Chavez <christiancchavez10@hotmail.com>
2020-03-29 17:29:17 -0500

    add logic to show date on Hero component

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -1,12 +1,21 @@
 import React from 'react';
 const Hero = ({ filters }) => {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+
     return (
         <section className="hero is-primary">
             <div className="hero-body">
                 <div className="container">
                 <h1 className="title">Hoteles</h1>
                 <h2 className="subtitle">
-                    desde el <strong>, DD de mmmm de AAAA</strong> hasta el <strong>, DD de mmmm de AAAA</strong>
+                    desde el <strong>, {filters.dateFrom.toLocaleDateString(undefined, options)}</strong> hasta el <strong>, {filters.dateTo.toLocaleDateString(undefined, options)}</strong> 
+                    {
+                        !filters.rooms &&  !filters.price && !filters.country ? 
+                        null : 
+                        <span>
+                            en <strong>{filters.country ? filters.country : null}</strong> por <strong>{filters.price? filters.price : null}</strong> de hasta <strong>{filters.rooms ? filters.rooms : null}</strong>
+                        </span>
+                    }
                 </h2>
                 </div>
             </div>
