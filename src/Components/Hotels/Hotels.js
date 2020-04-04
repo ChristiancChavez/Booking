5ece98d41f7c8c750139e425bf6f03d84dd97810
Christian Chavez <christiancchavez10@hotmail.com>
2020-04-04 16:39:46 -0500

    fix Hotels component

--- src/Components/Hotels/Hotels.js
+++ src/Components/Hotels/Hotels.js
@@ -1,28 +1,38 @@
 import React from 'react';
 import Hotel from '../Hotel/Hotel';
+import PropTypes from 'prop-types';
 
-const Hotels = ({ data }) => {
-    const hotelsFiltered = data.map( hotel => {
-        return <div className="column is-one-third">
-                    <Hotel data={ hotel } />
-                </div>
-    });
-    
+const Hotels = ({ hotels }) => {
     return (
-        <section className="section" style={ {marginTop: '3em'} }>
+        <section className="section" style={{ marginTop: '3em' }}>
         <div className="container">
             <div className="columns is-multiline">
-            { hotelsFiltered }
+                { hotels.length === 0 ? 
+                    (
+                        <article className="message is-warning">
+                            <div className="message-body">
+                                No hay hoteles que coincidan con tus criterios de busqueda.
+                            </div>
+                        </article>
+                    ) : 
+                    (
+                        hotels.map((hotel) => {
+                            return (
+                                <div key={ hotel.slug } className="column is-one-third">
+                                    <Hotel hotel={ hotel } />
+                                </div>
+                            );
+                        })
+                    )
+                }
             </div>
         </div>
-        {/* <article className="message is-warning">
-            <div className="message-body">
-                No se han encontrado hoteles que coincidan con los parámetros de búsqueda.
-            </div>
-        </article> */}
     </section>
     )
-    
 }
+
+Hotels.propTypes = {
+    hotels: PropTypes.array.isRequired
+};
 
 export default Hotels;
