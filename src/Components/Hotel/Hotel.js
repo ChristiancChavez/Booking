3b734cf354ad779befae32c8f6dc3d6755df3257
Christian Chavez <christiancchavez10@hotmail.com>
2020-04-04 16:51:24 -0500

    fix hitel component

--- src/Components/Hotel/Hotel.js
+++ src/Components/Hotel/Hotel.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import PropTypes from 'prop-types';
 
 const Hotel = ({ data }) => {
     return (
         <div className="card">
             <div className="card-image">
                 <figure className="image is-4by3">
-                    <img src={data.photo} alt="Sainte Jeanne Boutique & Spa" />
+                    <img src={data.photo} alt={data.slug} />
                 </figure>
             </div>
             <div className="card-content">
@@ -14,13 +17,17 @@ const Hotel = ({ data }) => {
                 <div className="field is-grouped is-grouped-multiline" style={{ marginTop: '1em' }}>
                     <div className="control">
                         <div className="tags has-addons">
-                            <span className="tag is-medium is-info"><i className="fas fa-map-marker"></i></span>
+                            <span className="tag is-medium is-info">
+                                <FontAwesomeIcon icon={fas.faMapMarker} />
+                            </span>
                             <span className="tag is-medium">{ `${ data.city }, ${ data.country }` }</span>
                         </div>
                     </div>
                     <div className="control">
                         <div className="tags has-addons">
-                            <span className="tag is-medium is-info"><i className="fas fa-bed"></i></span>
+                            <span className="tag is-medium is-info">
+                                <FontAwesomeIcon icon={fas.faBed} /> 
+                            </span>
                             <span className="tag is-medium">{ data.rooms }</span>
                         </div>
                     </div>
@@ -42,5 +49,9 @@ const Hotel = ({ data }) => {
         </div>
     )
 }
+
+Hotel.propTypes = {
+    hotel: PropTypes.object.isRequired
+};
 
 export default Hotel;
